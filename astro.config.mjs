a3b9565b0c11adc9365767e99ed4578666c7fe5f
andrewlau <andrewlau624@gmail.com>
2025-05-15 04:01:41 -0700

    Build navbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://andrewlau624.github.io',
-    base: '',
-  })
+  site: "https://andrewlau624.github.io",
+  base: "",
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
