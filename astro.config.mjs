bc4b1855bbf7bf46b0518b56df5186192898a4b7
andrewlau <andrewlau624@gmail.com>
2025-05-14 23:53:14 -0700

    Update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://andrewlau624.github.io',
-    base: 'my-portfolio',
+    base: '',
   })
