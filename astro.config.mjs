7cfcfb6a4ae4a781b802d7ce29f98e57938cf739
andrewlau <andrewlau624@gmail.com>
2025-05-15 23:17:19 -0700

    Obfuscate emails

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,17 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import mailObfuscation from "astro-mail-obfuscation";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://andrewlau624.github.io",
   base: "",
+
   vite: {
     plugins: [tailwindcss()],
   },
-  server: { host: true, port: 3000 }
+
+  server: { host: true, port: 3000 },
+  integrations: [mailObfuscation()]
 });
